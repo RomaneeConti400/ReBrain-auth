96f825b8e2b77c4edff9d6ac13e91cab41b673d5
Sergey Stykalskiy <romaneeconti400@mail.ru>
2024-08-06 13:28:32 +0300

    init: created the basic structure

---
+++ src/test/java/com/example/rebrainauth/ReBrainAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rebrainauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReBrainAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
