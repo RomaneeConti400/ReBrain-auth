225e1dfbff1328172a144c7774a21d18a15b1876
Sergey Stykalskiy <romaneeconti400@mail.ru>
2024-08-07 12:53:34 +0300

    feat: created models

---
+++ src/main/java/com/example/rebrainauth/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.example.rebrainauth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+}
