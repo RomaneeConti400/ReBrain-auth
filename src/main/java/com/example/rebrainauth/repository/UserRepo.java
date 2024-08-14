698c00e3b8326127835ae6bb7a208a014ccce291
Sergey Stykalskiy <romaneeconti400@mail.ru>
2024-08-14 15:38:31 +0300

    fix: registration returns void

--- src/main/java/com/example/rebrainauth/repository/UserRepo.java
+++ src/main/java/com/example/rebrainauth/repository/UserRepo.java
@@ -7,5 +7,4 @@ import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<UserEntity, String> {
     Optional<UserEntity> findByEmail(String email);
-    boolean existsByEmail(String email);
 }
